7c471d71958013040d7a0488608a7b4ef0f32a3e
Ben Galewsky <bengal1@illinois.edu>
2020-05-15 11:21:47 -0500

    Add MLFlow logging

--- sanetrain/workflow_builder/template_mlflow.py
+++ sanetrain/workflow_builder/template_mlflow.py
@@ -1,4 +1,7 @@
 main_loop = '''import sanetrain
+import mlflow
+import mlflow.pytorch
+
 import torch
 import torch.nn
 import torch.nn.functional as F
@@ -9,34 +12,39 @@ device = sanetrain.device.device
 
 {data_unpacker}
 
-train_loader = DataLoader(train_dataset, {train_loader_params})
-test_loader = DataLoader(test_dataset, {test_loader_params})
+with mlflow.start_run() as run:
+    train_loader = DataLoader(train_dataset, {train_loader_params})
+    test_loader = DataLoader(test_dataset, {test_loader_params})
+    
+    train_step = {train_step}
+    
+    train_epoch = {train_epoch}
+    
+    test_step = {test_step}
+    
+    test_epoch = {test_epoch}
+    
+    display_stats = {display_stats}
+    
+    {model_import}
+    model = {model_name}({model_params}).to(device)
+    
+    optimizer = {optimizer}(model.parameters(), {optimizer_params})
+    
+    lossf = {loss_function}
+    
+    for epoch in range(1, {epochs} + 1):
+        train_epoch(optimizer, model, lossf, train_loader, data_unpacker,
+                    train_step, epoch=epoch, device=device,
+                    {train_kwargs})
+    
+        test_stats = test_epoch(model, lossf, test_loader, data_unpacker,
+                                test_step, epoch=epoch, device=device,
+                                {test_kwargs})
+    
+        mlflow.log_metric("loss", test_stats["loss"], step=epoch)
+        mlflow.log_metric("test_acc", test_stats["correct"]/test_stats["total"], step=epoch)
 
-train_step = {train_step}
-
-train_epoch = {train_epoch}
-
-test_step = {test_step}
-
-test_epoch = {test_epoch}
-
-display_stats = {display_stats}
-
-{model_import}
-model = {model_name}({model_params}).to(device)
-
-optimizer = {optimizer}(model.parameters(), {optimizer_params})
-
-lossf = {loss_function}
-
-for epoch in range(1, {epochs} + 1):
-    train_epoch(optimizer, model, lossf, train_loader, data_unpacker,
-                train_step, epoch=epoch, device=device,
-                {train_kwargs})
-
-    test_stats = test_epoch(model, lossf, test_loader, data_unpacker,
-                            test_step, epoch=epoch, device=device,
-                            {test_kwargs})
-
-    display_stats(**test_stats)
+        display_stats(**test_stats)
+    mlflow.pytorch.log_model(model, "models")
 '''
