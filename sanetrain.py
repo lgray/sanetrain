f700d59b9deac9db8e2fcc54920a1ff6cf09645c
Ben Galewsky <ben@peartreestudio.net>
2020-05-18 11:21:30 -0500

    Migrate to jinja2 templates

--- sanetrain.py
+++ sanetrain.py
@@ -2,18 +2,26 @@ import argparse
 import subprocess
 from uuid import uuid1
 
+import yaml
+from jinja2 import Environment, PackageLoader
+
 from sanetrain.workflow_builder import generate_training
-from sanetrain.workflow_builder import template_mlflow
 
 parser = argparse.ArgumentParser(description='Run SaneTrain.')
 parser.add_argument("--file", help="Path to training yaml file")
 args = parser.parse_args()
 
-train_script = generate_training(template_mlflow.main_loop, args.file)
-fname = 'tests/%s.py' % uuid1().hex
+env = Environment(loader=PackageLoader('sanetrain', 'templates'))
+template = env.get_template('template_mlflow.py')
 
-with open(fname, 'w+') as fout:
-    fout.write(train_script)
+with open(args.file) as f:
+    config = yaml.load(f, Loader=yaml.SafeLoader)
 
-subprocess.run(["python", fname])
+    train_script = generate_training(template, config)
+    fname = 'tests/%s.py' % uuid1().hex
+
+    with open(fname, 'w+') as fout:
+        fout.write(train_script)
+
+    subprocess.run(["python", fname])
 
